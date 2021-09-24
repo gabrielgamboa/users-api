3ab1da926417d35023595a0e74c30b96b6cceedb
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-09-24 11:46:10 -0300

    implementando listagem de usuários caso usuario for admin

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -55,7 +55,7 @@ class UsersRepository implements IUsersRepository {
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const isUserAdmin = this.usersRepository.findById(user_id).admin;
+
+    if (isUserAdmin) {
+      const users = this.usersRepository.list();
+      return users;
+    }
+
+    throw new Error("You don't have permission to list all users.");
+    
   }
 }
 
